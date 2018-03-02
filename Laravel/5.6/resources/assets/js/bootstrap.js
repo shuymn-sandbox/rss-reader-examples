f1b7f87e6ce589e98db8e53bd05459bdd7245525
shuymn <mail@shuymn.me>
2018-03-03 01:55:46 +0900

    fix: remove global scopes

--- Laravel/5.6/resources/assets/js/bootstrap.js
+++ Laravel/5.6/resources/assets/js/bootstrap.js
@@ -1,19 +1,13 @@
-import _ from 'lodash';
 import * as axios from "axios";
 
-// set global variables
-window._ = _;
-window.axios = axios;
-
 // configure
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 // CSRF-TOKEN
 const token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+  axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+  console.error("CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token");
 }
-
