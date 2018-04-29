a7b6fb215bf10c60192b8df19f540d2c6cf61908
shuymn <mail@shuymn.me>
2018-04-29 14:54:28 +0900

    feat(laravel5.6): add the model of entries

---
+++ Laravel/5.6/app/Models/Entry.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entry extends Model
+{
+    protected $table = 'entries';
+
+    protected $fillable = [
+        'feed_id',
+        'title',
+        'link',
+        'description',
+        'entry_published_at',
+        'entry_updated_at'
+    ];
+}
