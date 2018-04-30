15669ccb6bcf43741ca062ca88fe55bb6d1f0454
shuymn <mail@shuymn.me>
2018-05-01 03:21:50 +0900

    feat(laravel5.6): add a locale file

---
+++ Laravel/5.6/resources/lang/en/messages.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'header' => [
+        'add' => 'Add',
+        'logout' => 'Log out',
+        'register' => 'Sign up',
+        'login' => 'Log in',
+    ],
+
+    'feed' => [
+        'create' => [
+            'url' => 'URL',
+            'title' => 'Title',
+            'post' => 'Post',
+        ],
+    ],
+
+    'login' => [
+        'email' => 'E-Mail Address',
+        'password' => 'Password',
+        'remember' => 'Remember Me',
+        'submit' => 'LogIn',
+    ],
+
+    'signup' => [
+        'username' => 'User Name',
+        'nickname' => 'Nick Name',
+        'email' => 'E-Mail Address',
+        'password' => 'Password',
+        'password-confirmation' => 'Password Confirmation',
+        'submit' => 'SignUp',
+    ],
+];
