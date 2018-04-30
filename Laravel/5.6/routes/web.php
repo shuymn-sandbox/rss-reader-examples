f2366a67b2618ae84d5f9e474ad89e6860e22dba
shuymn <mail@shuymn.me>
2018-05-01 03:28:25 +0900

    feat(laravel5.6): update routes

--- Laravel/5.6/routes/web.php
+++ Laravel/5.6/routes/web.php
@@ -1,19 +1,58 @@
 <?php
+declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\{
+    Index, Auth, Feed
+};
+use Illuminate\Routing\Router;
 
 /** @var \Illuminate\Routing\Router $router */
 
-$router->get('/', 'IndexController@index')->name('home');
+$router->group(['middleware' => ['web']], function (Router $router) {
+    $router->get('/', [
+        'as' => 'home',
+        'uses' => Index::class
+    ]);
 
-$router->auth();
+    $router->get('/feed', [
+        'as' => 'feed',
+        'uses' => Feed\GetFeed::class
+    ]);
+});
+
+$router->group(['middleware' => ['web', 'auth.inValid']], function (Router $router) {
+    $router->get('/signup', [
+        'as' => 'signup',
+        'uses' => Auth\SignUp\GetSignUp::class
+    ]);
+
+    $router->post('/signup', [
+        'uses' => Auth\SignUp\PostSignUp::class
+    ]);
+
+    $router->get('/login', [
+        'as' => 'login',
+        'uses' => Auth\LogIn\GetLogIn::class
+    ]);
+
+    $router->post('/login', [
+        'uses' => Auth\LogIn\PostLogIn::class
+    ]);
+});
+
+$router->group(['middleware' => ['web', 'auth.valid']], function (Router $router) {
+    $router->get('/logout', [
+        'as' => 'logout',
+        'uses' => Auth\LogOut\GetLogOut::class
+    ]);
+
+    $router->post('/feed', [
+        'uses' => Feed\PostFeed::class
+    ]);
+
+    $router->get('/feed/create', [
+        'as' => 'feed.create',
+        'uses' => Feed\Create\GetCreate::class
+    ]);
+});
 
