2f65c7cca5bcb517046a38da07f77cacd0dd2376
shuymn <mail@shuymn.me>
2018-05-09 05:51:10 +0900

    feat(laravel5.6): update english locales

---
+++ Laravel/5.6/resources/lang/en/EnglishConst.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+class EnglishConst
+{
+    public const
+        SIGN_UP = 'Sign Up',
+        PASSWORD = 'Password',
+        E_MAIL = 'E-Mail Address';
+}
--- Laravel/5.6/resources/lang/en/messages.php
+++ Laravel/5.6/resources/lang/en/messages.php
@@ -1,34 +1,37 @@
 <?php
+declare(strict_types=1);
 
 return [
     'header' => [
         'add' => 'Add',
-        'logout' => 'Log out',
-        'register' => 'Sign up',
-        'login' => 'Log in',
+        'logout' => 'Logout',
+        'register' => \EnglishConst::SIGN_UP,
+        'login' => 'Login',
     ],
 
     'feed' => [
         'create' => [
+            'page-title' => 'Create',
             'url' => 'URL',
-            'title' => 'Title',
             'post' => 'Post',
+            'failure' => 'Invalid URL given.',
         ],
     ],
 
     'login' => [
-        'email' => 'E-Mail Address',
-        'password' => 'Password',
+        'email' => \EnglishConst::E_MAIL,
+        'password' => \EnglishConst::PASSWORD,
         'remember' => 'Remember Me',
-        'submit' => 'LogIn',
+        'submit' => 'Log In',
     ],
 
     'signup' => [
+        'title' => \EnglishConst::SIGN_UP,
         'username' => 'User Name',
         'nickname' => 'Nick Name',
-        'email' => 'E-Mail Address',
-        'password' => 'Password',
+        'email' => \EnglishConst::E_MAIL,
+        'password' => \EnglishConst::PASSWORD,
         'password-confirmation' => 'Password Confirmation',
-        'submit' => 'SignUp',
+        'submit' => \EnglishConst::SIGN_UP,
     ],
 ];
